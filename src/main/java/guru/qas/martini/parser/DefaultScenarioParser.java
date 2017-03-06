631325820e7c45967de50f654e11ec1f51a349e2
pcurich <pcurich@digitalmeasures.com>
2017-03-06 16:03:45 -0600

    Removing unused Charset.

--- src/main/java/guru/qas/martini/parser/DefaultScenarioParser.java
+++ src/main/java/guru/qas/martini/parser/DefaultScenarioParser.java
@@ -4,25 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.nio.charset.Charset;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.core.io.Resource;
 
-@Configurable
 class DefaultScenarioParser implements ScenarioParser {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DefaultScenarioParser.class);
-
-	protected final Charset charset;
-
-	@Autowired
-	DefaultScenarioParser(Charset charset) {
-		this.charset = charset;
-	}
 
 	@Override
 	public Iterable<Scenario> getScenarios(Resource resource) throws IOException {
@@ -42,7 +31,7 @@ class DefaultScenarioParser implements ScenarioParser {
 	protected Feature getFeature(Resource resource, LineNumberReader reader) throws IOException {
 		FeatureLineProcessor processor = new FeatureLineProcessor(resource);
 
-		String line = null;
+		String line;
 		do {
 			reader.mark(Integer.MAX_VALUE);
 			line = reader.readLine();
