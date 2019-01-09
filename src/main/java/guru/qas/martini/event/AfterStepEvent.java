3d912cb92dc943ea3500ed72ff849e4b003076a2
pcurich <pcurich@digitalmeasures.com>
2019-01-09 15:29:48 -0600

    Updating step events.

--- src/main/java/guru/qas/martini/event/AfterStepEvent.java
+++ src/main/java/guru/qas/martini/event/AfterStepEvent.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Penny Rohr Curich
+Copyright 2019 Penny Rohr Curich
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,9 +16,17 @@ limitations under the License.
 
 package guru.qas.martini.event;
 
+import java.util.List;
+
 import org.springframework.core.ResolvableType;
 
+import com.google.common.collect.Iterables;
+
+import gherkin.ast.Step;
+import guru.qas.martini.Martini;
+import guru.qas.martini.MartiniException;
 import guru.qas.martini.result.MartiniResult;
+import guru.qas.martini.result.StepResult;
 
 public class AfterStepEvent extends MartiniScenarioEvent {
 
@@ -29,5 +37,17 @@ public class AfterStepEvent extends MartiniScenarioEvent {
 	@Override
 	public ResolvableType getResolvableType() {
 		return ResolvableType.forClass(AfterStepEvent.class);
+	}
+
+	public Step getStep() {
+		MartiniResult payload = super.getPayload();
+		List<StepResult> stepResults = payload.getStepResults();
+		StepResult last = Iterables.getLast(stepResults, null);
+		if (null == last) {
+			Martini martini = payload.getMartini();
+			String id = martini.getId();
+			throw new MartiniException(StepEventMessages.EMPTY_RESULTS, id);
+		}
+		return last.getStep();
 	}
 }
--- src/main/java/guru/qas/martini/event/BeforeStepEvent.java
+++ src/main/java/guru/qas/martini/event/BeforeStepEvent.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Penny Rohr Curich
+Copyright 2017-2019 Penny Rohr Curich
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,9 +16,22 @@ limitations under the License.
 
 package guru.qas.martini.event;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.core.ResolvableType;
 
+import com.google.common.collect.Lists;
+
+import gherkin.ast.Step;
+import guru.qas.martini.Martini;
+import guru.qas.martini.MartiniException;
 import guru.qas.martini.result.MartiniResult;
+import guru.qas.martini.result.StepResult;
+import guru.qas.martini.step.StepImplementation;
+
+import static guru.qas.martini.event.StepEventMessages.*;
 
 public class BeforeStepEvent extends MartiniScenarioEvent {
 
@@ -29,5 +42,42 @@ public class BeforeStepEvent extends MartiniScenarioEvent {
 	@Override
 	public ResolvableType getResolvableType() {
 		return ResolvableType.forClass(BeforeStepEvent.class);
+	}
+
+	public Step getStep() {
+		List<Step> steps = getSteps();
+		int index = getStepIndex();
+		return getStep(steps, index);
+	}
+
+	private List<Step> getSteps() throws MartiniException {
+		Martini martini = getMartini();
+		Map<Step, StepImplementation> stepIndex = martini.getStepIndex();
+		if (stepIndex.isEmpty()) {
+			throw new MartiniException(EMPTY_STEPS, martini.getId());
+		}
+		Set<Step> steps = stepIndex.keySet();
+		return Lists.newArrayList(steps);
+	}
+
+	private Martini getMartini() {
+		MartiniResult martiniResult = super.getPayload();
+		return martiniResult.getMartini();
+	}
+
+	private int getStepIndex() {
+		MartiniResult payload = super.getPayload();
+		List<StepResult> stepResults = payload.getStepResults();
+		return stepResults.size();
+	}
+
+	private Step getStep(List<Step> steps, int index) {
+		int stepCount = steps.size();
+		if (index < 0 || index > stepCount - 1) {
+			Martini martini = getMartini();
+			String id = martini.getId();
+			throw new MartiniException(STEP_RESULT_COUNT_MISMATCH, id, stepCount, index);
+		}
+		return steps.get(index);
 	}
 }
